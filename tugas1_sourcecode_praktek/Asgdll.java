ef747b555c3cedbeed52765b8229189ea1142eea
faudiahanwarr <125825171+faudiahanwarr@users.noreply.github.com>
2023-03-12 00:02:42 +0800

    Add files via upload

---
+++ tugas1_sourcecode_praktek/Asgdll.java
@@ -0,0 +1,18 @@
+/* NIM : 13020210145
+Nama : Faudiah Anwar
+Hari/Tanggal : Selasa,7 maret
+Waktu pengerjaan : 16.38 */
+
+public class Asgdll {
+
+    public static void main(String[] args){
+        //TODO Auto_generated method stub
+        /* Kamus */
+        float f= 20.0f;
+        double fll;
+
+        /* Algoritma */
+        fll=10.0f;
+        System.out.println ("f : "+f+ "\nf11: "+fll);
+    }
+}
---
+++ tugas1_sourcecode_praktek/Asign.java
@@ -0,0 +1,17 @@
+/* NIM : 13020210145
+Nama : Faudiah Anwar
+Hari/Tanggal : Selasa,7 maret
+Waktu pengerjaan : 16.38 */
+
+public class Asign {
+
+public static void main (String[] args){
+    // TODO Auto-generated method stub
+    /* Kamus */
+    int i;
+    /* Program */
+    System.out.print ("hello\n"); i = 5;
+    System.out.println ("Ini nilai i : " + i);
+    }
+
+}
